67541e79981b49c71f9c1211be48845fdf6965b1
msprague1106 <msprague@live.com>
2026-05-09 13:07:45 +0000

    made updates

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,19 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() 
+{
+    string name;
+
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "\nHello, " << name << "!" << endl;
+    cout << "\nWelcome to C++ programming!" << endl;
+    cout << endl;
+
+    return 0;
+}
