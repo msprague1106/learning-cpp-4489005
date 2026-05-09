44e1e8752d7f524edc861385ee04260476b1646c
msprague1106 <msprague@live.com>
2026-05-09 13:47:56 +0000

    update to code

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -3,10 +3,34 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
+using namespace std;
 
-int main(){
-    std::cout << "Hi There!" << std::endl;
+int a, b = 5; // string line comments
+
+int main()
+{
+    bool my_flag; 
+    unsigned int postive;
+
+    a = 7;
+
+    my_flag = false;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "flag = " << my_flag << endl;
+
+    my_flag = true;
+    cout << "flag = " << my_flag << endl;
+    cout << "a + b = " << a + b << endl;    
+    cout << "b - a = " << b - a << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;   
     
-    std::cout << std::endl << std::endl;
+    postive = b - a;
+    cout << "b - a (unsigned) =" << postive << endl;
+    
+    cout << endl << endl;
+
     return (0);
 }
